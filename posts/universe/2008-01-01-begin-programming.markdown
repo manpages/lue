03f8bffbf78d871e3c98e71d0f5fefad029c2f72
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-01-07 19:15:58 +0200

    Initial blog version

---
+++ posts/universe/2008-01-01-begin-programming.markdown
@@ -0,0 +1,5 @@
+---
+title: How I started programming
+---
+
+I liked computers and decided to make them do what I want.
--- site.hs
+++ site.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-
+import           Control.Monad (forM_)
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -15,47 +15,52 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match (fromList ["about.rst", "contact.markdown", "index.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
+    forM_ [ "ru", "life", "universe" ] $ \x ->
+      match (fromGlob $ "posts/" ++ x ++ "/*") $ do
+          route $ setExtension "html"
+          compile $ pandocCompiler
+              >>= loadAndApplyTemplate "templates/post.html"    postCtx
+              >>= loadAndApplyTemplate "templates/default.html" postCtx
+              >>= relativizeUrls
+
+    forM_ [ "ru", "life", "universe" ] $ \x ->
+      create [x] $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        compile $ do
+            posts <- recentFirst =<< loadAll (fromGlob $ "posts/" ++ (show x) ++ "/*")
+            let archiveCtx =
+                    listField  (show x) postCtx (return posts)    `mappend`
+                    constField "title"  (title $ show x)          `mappend`
+                    defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html"   archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            life     <- recentFirst =<< loadAll "posts/life/*"
+            universe <- recentFirst =<< loadAll "posts/universe/*"
+            russian  <- recentFirst =<< loadAll "posts/ru/*"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+                    listField  "life"     postCtx (return life)     `mappend`
+                    listField  "universe" postCtx (return universe) `mappend`
+                    listField  "ru"       postCtx (return russian)  `mappend`
+                    constField "title"    "Everything"              `mappend`
                     defaultContext
-
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
@@ -65,3 +70,7 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+title "ru" = "Посты на русском языке"
+title "life" = "Life"
+title "universe" = "Universe"
